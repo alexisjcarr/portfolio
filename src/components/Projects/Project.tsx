37b0699e9d68af78e541bc6962baf02847248eab
alexisjcarr <acarr59@gmail.com>
2019-09-26 13:30:25 -0500

    💄 Makes responsive
     #1

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+
+import "./ProjectsList.scss";
+
+export interface ProjectProps {
+  projectName: string;
+  deploymentLink?: string;
+  githubRepo?: string;
+  role: string;
+  gif: string;
+  description: string;
+  techStack: string;
+  imgClass: string;
+}
+
+const Project: React.SFC<ProjectProps> = ({
+  projectName,
+  deploymentLink,
+  githubRepo,
+  role,
+  gif,
+  description,
+  techStack,
+  imgClass
+}) => {
+  return (
+    <>
+      <br />
+      <div className="project">
+        <h3>
+          {projectName}{" "}
+          <a href={deploymentLink} target="_blank" rel="noopener noreferrer">
+            <i className="iconP fas fa-link"></i>
+          </a>{" "}
+          <a href={githubRepo} target="_blank" rel="noopener noreferrer">
+            <i className="iconP fab fa-github"></i>
+          </a>
+        </h3>
+        <p>
+          <span style={{ textDecoration: "underline" }}>Role:</span> {role}
+        </p>
+        <img src={gif} alt="app" className={imgClass} />
+        <div className="desc-stack">
+          <p className="description">
+            <p style={{ textDecoration: "underline" }}>Description: </p>
+            {description}
+          </p>
+          <p className="stack">
+            <p style={{ textDecoration: "underline" }}>Tech Stack: </p>
+            {techStack}
+          </p>
+        </div>
+      </div>
+      <br />
+    </>
+  );
+};
+
+export default Project;
--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import Project from "./Project";
+
 import sleeptrackr from "../../sleeptrackr.gif";
 import demo from "../../rtsp.gif";
 import fitness from "../../fitness.gif";
@@ -7,111 +9,53 @@ import servable from "../../servable.gif";
 
 import "./ProjectsList.scss";
 
-export interface ProjectsListProps {}
-
-const ProjectsList: React.SFC<ProjectsListProps> = () => {
+const ProjectsList: React.SFC = () => {
   return (
     <div className="container" id="projects">
       <div className="projects-card">
         <h1>projects</h1>
-        <br />
-        <div className="project">
-          <img
-            src={sleeptrackr}
-            alt="sleeptrackr app"
-            className="project-img"
-          />
-          <h3>Sleep Tracker</h3>
-          <p>Front End Developer</p>
-          <p>
-            A web app that recommends how much sleep you need using user inputs
-            for mood and time slept.
-          </p>
-          <p>React | React Router | Redux | Material UI | Axios</p>
-          <a
-            href="https://sleeptrackr.netlify.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Deployed Link</p>
-          </a>
-          <a
-            href="https://github.com/alexisjcarr/Sleep-Tracker-Frontend"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Github Repo</p>
-          </a>
-        </div>
-        <br />
-        <div className="project">
-          <img src={fitness} alt="fitness app" className="project-img" />
-          <h3>Grit 'N Grind</h3>
-          <p>Front End Developer</p>
-          <p>
-            The first web application of its kind: Grit-N-Grind is a fitness
-            competition application that gyms can use to throw competitions at
-            their local gyms.
-          </p>
-          <p>React | React Hooks | React Context | Firebase</p>
-          <a
-            href="https://gritngrind.fitness/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Deployed Link</p>
-          </a>
-          <a
-            href="https://github.com/grit-n-grind/client"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Github Repo</p>
-          </a>
-        </div>
-        <br />
-        <div className="project">
-          <img
-            src={servable}
-            alt="community service app"
-            className="project-img-servable"
-          />
-          <h3>Servable</h3>
-          <p>Fullstack Developer</p>
-          <p>
-            Still under development. A mobile-friendly web app that matches users to volunteer
-            opportunities via a Tinder-like interface.
-          </p>
-          <p>React | React Hooks | Typescript | Node | Express | PostgreSQL</p>
-          <a>
-            <p>Deployed Link: TBA</p>
-          </a>
-          <a>
-            <p>Github Repo: TBA</p>
-          </a>
-        </div>
-        <br />
-        <div className="project">
-          <img src={demo} alt="realtime app" className="project-img" />
-          <h3>Real Time Data Renderer</h3>
-          <p>Front End Developer | Data Viz Engineer</p>
-          <p>TBA</p>
-          <p>React | React Hooks | SocketIO</p>
-          <a
-            href="http://realtime-viz-practice.surge.sh"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Deployed Link</p>
-          </a>
-          <a
-            href="https://github.com/alexisjcarr/socket-io-playground-client"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <p>Github Repo</p>
-          </a>
-        </div>
+        <Project
+          projectName="Sleep Tracker"
+          deploymentLink="https://sleeptrackr.netlify.com"
+          githubRepo="https://github.com/alexisjcarr/Sleep-Tracker-Frontend"
+          role="Front End Developer"
+          gif={sleeptrackr}
+          description="A web app that recommends how much sleep you need using user inputs for mood and time slept."
+          techStack="React | React Router | Redux | Material UI | Axios"
+          imgClass="project-img"
+        />
+        <hr />
+        <Project
+          projectName="Grit 'N Grind"
+          deploymentLink="https://gritngrind.fitness/"
+          githubRepo="https://github.com/grit-n-grind/client"
+          role="Front End Developer"
+          gif={fitness}
+          description="The first web application of its kind: Grit-N-Grind is a fitness
+          competition application that gyms can use to throw competitions at
+          their local gyms."
+          techStack="React | React Hooks | React Context | Firebase"
+          imgClass="project-img"
+        />
+        <hr />
+        <Project
+          projectName="Servable"
+          role="Fullstack Developer"
+          gif={servable}
+          description="Still under development. A mobile-friendly web app that matches
+          users to volunteer opportunities via a Tinder-like interface."
+          techStack="React | React Hooks | Typescript | Node | Express | PostgreSQL"
+          imgClass="project-img-servable"
+        />
+        <hr />
+        <Project
+          projectName="Real-time Data Renderer"
+          role="Front End Developer | Data Viz Engineer"
+          gif={demo}
+          description="TBA"
+          techStack="React | React Hooks | SocketIO"
+          imgClass="project-img"
+        />
       </div>
     </div>
   );
