3d5ba19cd2f60fb9cfe1dcf1cdb7578320a50522
alexisjcarr <acarr59@gmail.com>
2019-10-24 09:06:39 -0500

    💄 Changes spacing of skills in mobile view

---
+++ src/components/Resume/Resume.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Document } from "react-pdf";
+
+const Resume: React.SFC = () => (
+  <Document file="../../Carr_Alexis_resume.pdf" />
+);
+
+export default Resume;
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,8 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/App";
-import "./index.scss";
 
-import * as serviceWorker from "./serviceWorker";
+import "./index.scss";
 
 ReactDOM.render(
   <Router>
@@ -13,8 +12,3 @@ ReactDOM.render(
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
