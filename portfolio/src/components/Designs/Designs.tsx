1219caa130221e49f27070ce7c0d76f1b65fac5b
alexis.carr <alexis.carr@eventbrite.com>
2021-05-28 20:11:43 -0500

    Removes functions from tol

--- portfolio/src/components/Designs/Designs.tsx
+++ portfolio/src/components/Designs/Designs.tsx
@@ -17,33 +17,33 @@ const Designs: React.FC = () => (
       <div className="designs-card">
         <h1> DESIGNS </h1>
         <p>Hello world! Welcome to my corner of the internet.</p>
-        <p>Soon, I will start compiling systems designs and solutions architectures that I come up with in my free time. I am currently working on the following designs. When one is in progress, I will add a <span id="icon">⏳</span> next to the link. When one is ready, I will add a <span id="icon">✅</span> next to it. STAY TUNED!</p>
+        <p>Soon, I will start compiling systems designs and solutions architectures that I come up with in my free time. I am currently working on the following designs. When one is in progress, I will add a <span id="icon" role="img" aria-label="hourglass">⏳</span> next to the link. When one is ready, I will add a <span id="icon" role="img" aria-label="checkmark">✅</span> next to it. STAY TUNED!</p>
         <br />
-        <a href="">
-          <p><span id="icon">⏳</span> Code Deployment System</p>
+        <a href="#">
+          <p><span id="icon" role="img" aria-label="hourglass">⏳</span> Code Deployment System</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Stockbroker App</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Facebook News Feed</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Google Drive</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Reddit API</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Netflix</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Uber API</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Slack</p>
         </a>
-        <a href="">
+        <a href="#">
           <p>Airbnb</p>
         </a>
       </div>
